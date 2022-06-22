48b720b565338e33a9e68d1109838ecd3efffcec
Nominaru <lyes.r237@gmail.com>
2022-06-22 11:07:39 +0200

    Eslint + Prettier implement

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,12 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(express.static('src/public'))
+app.use(express.static('src/public'));
 
 app.get('/', (req, res) => {
-    res.render("index")
-  })
-  
-  app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
-  })
-  
+  res.render('index');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});

7194d0ad94af9fb629d4bbde0679c78c823c4a8e
ruzt99 <zuniga.tellez.rodrigo.uriel@hotmail.com>
2022-06-22 11:04:01 +0200

    Feat: Github Action, Typescript fix and codeowners file

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ const app = express();
 
 app.use(express.static('src/public'))
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
     res.render("index")
-  })
+})
   
-  app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Example app listening on port ${process.env.PORT}`)
-  })
+})
   

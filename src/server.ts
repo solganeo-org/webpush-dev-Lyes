8b3d788e233c0b4e7d07557deb0c2084d27bfe60
Nominaru <lyes.r237@gmail.com>
2022-06-22 11:19:45 +0200

    Eslint

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express');
 require('dotenv').config();
 
@@ -12,3 +13,20 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${process.env.PORT}`);
 });
+=======
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.static('src/public'))
+
+app.get('/', (req: any, res: any) => {
+    res.render("index")
+})
+  
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
+  
+>>>>>>> 1db9d6c7d98216f5a47c358bf3adb1a79f58709b
